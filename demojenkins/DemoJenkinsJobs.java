8eb409e0a6e1ff6258498007ffeb21ffa52f5b59
gopal <sbrajagopal26@gmail.com>
2016-10-25 17:43:07 +0200

    Jenkins demo java program

---
+++ demojenkins/DemoJenkinsJobs.java
@@ -0,0 +1,9 @@
+package demojenkins;
+
+//import org.testng.annotations.Test;
+public class DemoJenkinsJobs {
+	//@Test
+	public static void main(String[] args){
+		System.out.println("Welcome to jenkins world");
+	}
+}
